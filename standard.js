874b7701c0783b13085a8801b68d6e4edb9e58c1
jaguer0 <aguero.manuel@Yahoo.com>
2016-09-22 16:16:29 -0500

    i

--- standard.js
+++ standard.js
@@ -1,17 +1,17 @@
 
     window.sr = ScrollReveal();
     sr.reveal('.sr-icons', {
-        duration: 600,
+        duration: 1000,
         scale: 0.3,
         distance: '0px'
-    }, 700);
+    }, 1500);
     sr.reveal('.sr-resume', {
         duration: 1000,
         delay: 200
     });
     sr.reveal('.sr-icons2', {
-        duration: 600,
+        duration: 1000,
         scale: 0.3,
         distance: '0px'
-    }, 700);
+    }, 1500);
 
