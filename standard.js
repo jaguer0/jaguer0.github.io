c3990ded467ca9f65b82ddb1cd0619acc0f489bf
jaguer0 <aguero.manuel@Yahoo.com>
2016-09-22 15:25:11 -0500

    k

---
+++ standard.js
@@ -0,0 +1,17 @@
+
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 700);
+    sr.reveal('.sr-resume', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 670);
+
