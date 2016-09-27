3ca0c26851a1266fcda02fd94e1086008adaf8dc
jaguer0 <aguero.manuel@Yahoo.com>
2016-09-26 21:04:54 -0500

    update

--- standard.js
+++ standard.js
@@ -4,14 +4,9 @@
         duration: 600,
         scale: 0.3,
         distance: '0px'
-    }, 800);
+    }, 700);
     sr.reveal('.sr-resume', {
         duration: 800,
         delay: 200
     });
-    sr.reveal('.sr-icons2', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 800);
-
+   
