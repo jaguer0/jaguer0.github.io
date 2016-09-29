93722368cae7742ab73273f5769b2bd2323ed81c
jaguer0 <aguero.manuel@Yahoo.com>
2016-09-29 14:06:55 -0500

    d

--- standard.js
+++ standard.js
@@ -1,10 +1,15 @@
 
     window.sr = ScrollReveal();
+sr.reveal('.sr-arrow', {
+        duration: 1000,
+        delay: 200
+    });
     sr.reveal('.sr-icons', {
         duration: 600,
         scale: 0.3,
         distance: '0px'
     }, 700);
+
     sr.reveal('.sr-resume', {
         duration: 800,
         delay: 200
